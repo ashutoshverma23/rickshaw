d91edcad902b04fdcdb1d24ed3cc622e49624b47
Ashutosh Verma <ashutoshrgnict@gmail.com>
2024-08-01 17:21:50 +0530

    chore: Update backend server URL in Vite configuration file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000', // Replace with your backend server URL
+        target: 'https://rickshaw.onrender.com/', // Replace with your backend server URL
         changeOrigin: true,
       },
     },
